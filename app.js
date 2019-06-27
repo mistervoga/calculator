4a09a6b62093bea808f1304a09eecc77e27737b7
Nicolas von Garrel <nivoga@hotmail.de>
2019-06-27 21:45:53 +0200

    interface setup

---
+++ app.js
@@ -0,0 +1,12 @@
+//Basic Functions
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+
+function displayNum(num) {
+    const specific = numberbutton.target
+
+}
