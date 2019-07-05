8f4195ee427b7707d6c74c303f8048225fbcdbd2
Nicolas von Garrel <nivoga@hotmail.de>
2019-07-05 14:50:07 +0200

    final without decimal

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -8,7 +10,7 @@ function operate(operator, a, b) {
         case "+":
             return add(a, b);
         case "-":
-            return subtract(a, b);
+            return substract(a, b);
         case "*":
             return multiply(a, b);
         case "/":
@@ -22,38 +24,88 @@ const clear = document.querySelector(".delete-all");
 const operators = document.querySelectorAll(".operator");
 const decimal = document.querySelector(".decimal");
 
-const operands = [];
-const operator = [];
-const firstOperand = undefined;
-const onlyoperator = false;
+let first = 0;
+let second = 0;
+let values = [];
+let joinedvalues = [];
+let onlyoperator = false;
 
 numbers.forEach(number =>
-    number.addEventListener("click", e => {
-        if (operator.length > 0) {
-            display.value += e.target.value;
-            operands.splice(1, display.value.length - 1, display.value);
-        }
-        display.value += e.target.value;
-        operands.splice(0, display.value.length - 1, display.value);
+    number.addEventListener("click", function(e) {
+        first = e.target.textContent;
+        values.push(first);
+        console.log(values);
+        joinedvalues = values.join("");
+        console.log(joinedvalues);
+        display.value += e.target.textContent;
     })
 );
 
 operators.forEach(operator =>
-    operator.addEventListener("click", e => {
+    operator.addEventListener("click", function(e) {
         if (!isNaN(display.value)) {
             display.value += e.target.value;
             onlyoperator = true;
-            operator.push(display.value);
+            values.push(e.target.textContent);
+            console.log(values);
         }
 
         if (onlyoperator == true) {
             return;
         }
-        alert(operands.length);
-        alert(operator)
     })
 );
 
-equals.addEventListener("click", e => {
-    if (operands.length == 2 && operator.length == 1) {}
+let result = 0;
+
+equals.addEventListener("click", function(e) {
+    console.log(joinedvalues.length);
+    for (let i = 0; i < joinedvalues.length; i++) {
+        if (joinedvalues[i].includes("-")) {
+            let op = joinedvalues[i];
+            first = joinedvalues.slice(0, i);
+            console.log(first);
+            console.log(op);
+            second = joinedvalues.slice(i + 1, joinedvalues.length);
+            console.log(second);
+            result = operate(op, first, second);
+            display.value = "" + result;
+        }
+        if (joinedvalues[i].includes("/")) {
+            let op = joinedvalues[i];
+            first = joinedvalues.slice(0, i);
+            console.log(first);
+            console.log(op);
+            second = joinedvalues.slice(i + 1, joinedvalues.length);
+            console.log(second);
+            result = operate(op, first, second);
+            display.value = "" + result;
+        }
+        if (joinedvalues[i].includes("*")) {
+            let op = joinedvalues[i];
+            first = joinedvalues.slice(0, i);
+            console.log(first);
+            console.log(op);
+            second = joinedvalues.slice(i + 1, joinedvalues.length);
+            console.log(second);
+            result = operate(op, first, second);
+            display.value = "" + result;
+        }
+        if (joinedvalues[i].includes("+")) {
+            let op = joinedvalues[i];
+            first = joinedvalues.slice(0, i);
+            console.log(first);
+            console.log(op);
+            second = joinedvalues.slice(i + 1, joinedvalues.length);
+            console.log(second);
+            result = operate(op, parseFloat(first), parseFloat(second));
+            display.value = "" + result;
+        }
+    }
+});
+
+clear.addEventListener("click", function() {
+    display.value = "";
+    joinedvalues.length == 0;
+    values.length = 0;
 });
